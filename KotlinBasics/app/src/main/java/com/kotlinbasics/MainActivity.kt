948a175485aca27334e0ce793aed846ed6dbbb01
keumbyeol <bgbbgb1234@gmail.com>
2025-09-12 12:18:41 +0900

    week2:variables03

--- KotlinBasics/app/src/main/java/com/kotlinbasics/MainActivity.kt
+++ KotlinBasics/app/src/main/java/com/kotlinbasics/MainActivity.kt
@@ -61,6 +61,29 @@ private fun week02Variables(){
 
 }
 
+private fun week02Functions(){
+//    println("Week 02: Functions")
+//
+//    fun greet(name:String)="Hello, $name"
+//
+//    println(greet("Android developer")
+    println("== Kotlin Functions ==")
+
+    fun greet(name: String): String{
+        return "Hello, $name!"
+    }
+
+    fun add(a: Int, b: Int) = a + b
+
+    fun introduce(name: String, age: Int=19){
+        println("My name is $name and I'm $age years old")
+    }
+
+    println(greet("Kotlin"))
+    println("Sum :${add(5, -71)}")
+
+}
+
 @Composable
 fun Greeting(name: String, modifier: Modifier = Modifier) {
     Text(
