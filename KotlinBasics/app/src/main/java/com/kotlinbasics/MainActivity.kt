589bcdf827b673686fba8878ee600bf36415a7fd
keumbyeol <bgbbgb1234@gmail.com>
2025-09-19 11:55:07 +0900

    week03 : Log.d constructor

--- KotlinBasics/app/src/main/java/com/kotlinbasics/MainActivity.kt
+++ KotlinBasics/app/src/main/java/com/kotlinbasics/MainActivity.kt
@@ -1,6 +1,7 @@
 package com.kotlinbasics
 
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
@@ -30,8 +31,8 @@ class MainActivity : ComponentActivity() {
         }
         //week02Variables()
         //week02Functions()
-        //week03Classes()
-        week03Collections()
+        week03Classes()
+        //week03Collections()
     }
 }
 
@@ -90,30 +91,34 @@ private fun week02Functions(){
 }
 
 private fun week03Classes(){
-    println("==Kotlin Classes ==")
+    Log.d("Kotlinweek03","==Kotlin Classes==")
 
-    class Student{
-        var name: String = ""
-        var age: Int = 0
-
+    class Person(val name: String, var age:Int){
         fun introduce(){
-            println("Hi, I'm  $name and I'm $age years old ")
+            Log.d("Kotlinweek03", "안녕하세요, $name ($age 세)입니다")
+        }
+
+        fun birthday(){
+            age++
+            Log.d("Kotlinweek03", "$name 의 생일! 이제 $age 세...")
         }
     }
+    val person1 = Person("홍길동", 27)
+    person1.introduce()
+    person1.birthday()
 
-    val student = Student()
-    student.name = "Mirae"
-    student.age = 21
-    student.introduce()
-
-    data class Person(val name: String, val age: Int)
-
-    val person1 = Person("Kim", 23)
-    val person2 = Person("Kim", 23)
-
-    println("Person 1 : $person1")
-    println("Equal? ${person1 == person2}")
-
+    class Animal(var species: String){
+        var weight: Double = 0.0
+        constructor(species: String, weight: Double) : this(species){
+            this.weight = weight
+            Log.d("Kotlinweek03", "$species 의 무게 : $weight kg")
+        }
+        fun makeSound(){
+            Log.d("Kotlinweek03", "$species 가 소리를 냅니다.")
+        }
+    }
+    val puppy = Animal("웰시코기", 10.5)
+    puppy.makeSound()
 }
 
 private fun week03Collections(){
