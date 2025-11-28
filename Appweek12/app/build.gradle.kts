8aa38b7a0e9a23343e7bb98641fb441313db457c
keumbyeol <bgbbgb1234@gmail.com>
2025-11-28 12:01:26 +0900

    week12:Counter App v0.6(MVVM : StateFlow)

--- Appweek12/app/build.gradle.kts
+++ Appweek12/app/build.gradle.kts
@@ -9,7 +9,7 @@ android {
 
     defaultConfig {
         applicationId = "com.appweek12"
-        minSdk = 27
+        minSdk = 26
         targetSdk = 36
         versionCode = 1
         versionName = "1.0"
@@ -33,7 +33,6 @@ android {
     kotlinOptions {
         jvmTarget = "11"
     }
-
     buildFeatures{
         viewBinding = true
     }
@@ -46,6 +45,15 @@ dependencies {
     implementation(libs.material)
     implementation(libs.androidx.activity)
     implementation(libs.androidx.constraintlayout)
+
+    // ViewModel, StateFlow
+    val lifecycleVersion = "2.7.0"
+    val coroutineVersion = "1.7.3"
+    implementation("androidx.lifecycle:lifecycle-viewmodel-ktx:$lifecycleVersion")
+    implementation("androidx.lifecycle:lifecycle-runtime-ktx:${lifecycleVersion}")
+    implementation("androidx.activity:activity:1.8.1")
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:${coroutineVersion}")
+
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
